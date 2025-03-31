62e040c4bb5eb27f955b967b0ffbd3451a0909dd
Əbil Abilli <ebilebilli3@gmail.com>
2025-03-31 23:43:59 +0400

    fallowers model created

--- instagram/apps/fallowers/models.py
+++ instagram/apps/fallowers/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from users.models import CustomUser
 
-# Create your models here.
+class Follow(models.Model):
+    follower = models.ForeignKey(CustomUser, related_name='followings', on_delete=models.CASCADE)
+    following = models.ForeignKey(CustomUser, related_name='followers', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ['follower', 'following']
