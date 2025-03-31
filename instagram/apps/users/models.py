80ad3ca2dc5d18a665f348f11c73650e991e8a38
Əbil Abilli <ebilebilli3@gmail.com>
2025-03-31 23:28:49 +0400

    CustomUser model Created

--- instagram/apps/users/models.py
+++ instagram/apps/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    bio = models.TextField(max_length=155, null=True, blank=True)
+    profile_picture = models.ImageField(upload_to='profile/pictures', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
--- instagram/instagram/settings.py
+++ instagram/instagram/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+
 
     #apps
     'apps.fallowers',
@@ -128,3 +130,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#Media
+import os
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
